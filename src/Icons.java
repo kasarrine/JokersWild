63c97d8ee1a477728ffdd6922cb3a93b25cb1fab
avilesrichard <55413182+avilesrichard@users.noreply.github.com>
2020-11-21 18:23:40 -0600

    Update Icons.java

--- src/Icons.java
+++ src/Icons.java
@@ -13,10 +13,12 @@ import javafx.scene.control.ListCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/** This class helps control the player icon combobox */
 public class Icons {
 
     private final ComboBox<String> iconBox;
 
+    /** This class helps control the player icon combobox */
     public Icons() {
         String icon1 = "file:images/icons/icon1.png";
         String icon2 = "file:images/icons/icon2.png";
@@ -27,25 +29,32 @@ public class Icons {
         ObservableList<String> iconList = FXCollections.observableArrayList(icon1,
                 icon2, icon3, icon4, icon5, icon6);
         iconBox = new ComboBox<>(iconList);
-        iconBox.setCellFactory(pram -> new IconListCell());
-        iconBox.setButtonCell(new IconListCell());
+        iconBox.setCellFactory(param -> new IconListCell());
+        iconBox.setButtonCell(new IconListCell());              // combobox image update
     }
 
+    /** Updates combobox cell/list content */
     static class IconListCell extends ListCell<String> {
 
+        /*
+            When player selects and avatar icon, the content is taken and moved
+            to a specified position, so the list view becomes blank.
+            To prevent that, this method updates the combobox to maintain the content inside it.
+         */
+        
         @Override
-        protected void updateItem(String icon, boolean empty) {
+        protected void updateItem(String icon, boolean empty) {     // override updateItem for specified customization
             super.updateItem(icon, empty);
             setText(icon);
-            if(empty || icon == null) {
-                setGraphic(null);
+            if(empty || icon == null) {                             
+                setGraphic(null);                                   // clear image
             } else {
-                Image iconImage = new Image(icon);
-                ImageView iconImageView = new ImageView(iconImage);
-                iconImageView.setFitHeight(70);
+                Image iconImage = new Image(icon);                  // create new image of selected item
+                ImageView iconImageView = new ImageView(iconImage); 
+                iconImageView.setFitHeight(70); 
                 iconImageView.setFitWidth(60);
-                setGraphic(iconImageView);
-            }
+                setGraphic(iconImageView);                          // set the image
+            }   
             setText("");
         }
     }
